2e9f53c77a0600550e0391010faf9f200a533d7b
Yash Kumar Verma <yk.verma2000@gmail.com>
2021-02-07 23:04:09 +0530

    feat(app): add dependencies

--- app.js
+++ app.js
@@ -1,2 +1,12 @@
-const express = require("express")
-const app = express()
+const express = require("express");
+const app = express();
+
+const data = require("./data.json");
+
+app.get("./data", (req, res) => {
+  res.json(data);
+});
+
+app.listen(3000, () => {
+  console.log("listening on port 3000");
+});
