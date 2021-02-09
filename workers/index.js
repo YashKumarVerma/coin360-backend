56ebe5847ec7bab2b9dd71425901a674790f76e4
Yash Kumar Verma <yk.verma2000@gmail.com>
2021-02-09 20:30:13 +0530

    feat(logger): convery hydrate to user

--- workers/index.js
+++ workers/index.js
@@ -1,6 +1,6 @@
 const data = require("../data.json");
 const _ = require("lodash");
-
+const { user } = require("../logger/logger");
 const marketData = data.markets;
 
 /**
@@ -9,7 +9,7 @@ const marketData = data.markets;
  * @param {number} end end index of data to send
  */
 const hydrate = (start, end) => {
-  console.log(`hydrating page from ${start} to ${end}`);
+  user(`hydrating page from ${start} to ${end}`);
   return marketData.filter((value, index) => {
     return index >= start && index <= end;
   });
