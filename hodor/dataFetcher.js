8def342b7142287f4ef8acbbdf19e5424cb32f3c
Yash Kumar Verma <yk.verma2000@gmail.com>
2021-02-09 23:04:36 +0530

    feat(logger): write dataMapper

--- hodor/dataFetcher.js
+++ hodor/dataFetcher.js
@@ -1,9 +1,12 @@
 const axios = require("axios").default;
 const { CONFIG } = require("../config");
+const { hodor } = require("../logger/logger");
 
 const dataFetcher = async () => {
+  hodor(`loading data from ${CONFIG.api}`);
   const { data } = await axios.get(CONFIG.api);
-  return data;
+  hodor(`loaded data from ${CONFIG.api}`);
+  return data.markets;
 };
 
 module.exports = { dataFetcher };
